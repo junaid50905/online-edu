ed8789d37de375640029fa9de333da5b03f66990
junaid hossain <junaidhossain50905@gmail.com>
2023-05-18 13:19:04 +0600

    addding authentication

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,10 +18,23 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
+
+
 
 Route::get('/student/dashboard', function () {
     return view('student.dashboard');
-});
+})->middleware('auth');
 
 Route::get('/student/dashboard/courses', function () {
     return view('student.course.courses');
